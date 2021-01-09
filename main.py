6d3c0706183a2cfa131faf994ae6fa467a4eba86
Adrian Ishmael <adrianrayishmael@gmail.com>
2021-01-09 01:15:19 -0600

    Now creates a window with text using layout

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import sys
+from PySide6.QtGui import QWindow
+from PySide6.QtWidgets import QApplication
+from PySide6.QtWidgets import QLabel
+from PySide6.QtWidgets import QVBoxLayout
+from PySide6.QtWidgets import QWidget
+
 if __name__ == "__main__":
-    print("Hello World")
+    app = QApplication(sys.argv)
+    window = QWidget()
+    window.setWindowTitle("Hydra")
+    window.resize(200, 50)
+
+    label = QLabel(text="Cut off one head . . .")
+    layout = QVBoxLayout()
+    layout.addWidget(label)
+
+    window.setLayout(layout)
+    window.show()
+
+    sys.exit(app.exec_())
