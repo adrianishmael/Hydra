98cb5a0c47a4d69a7d5db464b0c74e36a0c69cc5
Adrian Ishmael <adrainrayishmael@gmail.com>
2021-06-23 20:19:27 -0500

    Working. Creates two windows when closing one

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
 import sys
-from PySide6.QtGui import QWindow
-from PySide6.QtWidgets import QApplication
-from PySide6.QtWidgets import QLabel
-from PySide6.QtWidgets import QVBoxLayout
-from PySide6.QtWidgets import QWidget
+import random
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = QWidget()
-    window.setWindowTitle("Hydra")
-    window.resize(200, 50)
+from PySide6 import QtWidgets
+from PySide6.QtGui import QCloseEvent
 
-    label = QLabel(text="Cut off one head . . .")
-    layout = QVBoxLayout()
-    layout.addWidget(label)
+app = QtWidgets.QApplication(sys.argv)
+width = app.primaryScreen().size().width()
+height = app.primaryScreen().size().height()
+windows = []
 
-    window.setLayout(layout)
+
+class Hydra(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.text = QtWidgets.QLabel("Cut off one head . . .")
+
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+
+    def closeEvent(self, _e: QCloseEvent):
+        create_window()
+        create_window()
+
+
+def create_window():
+    window = Hydra()
+    window.move(random.randint(0, width), random.randint(0, height))
     window.show()
+    windows.append(window)
 
-    sys.exit(app.exec())
+
+create_window()
+sys.exit(app.exec())
