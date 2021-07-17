1dc879962ca07700c0dba3cc21e806202a0e8c32
Adrian Ishmael <adrianrayishmael@gmail.com>
2021-07-17 00:29:17 -0500

    slight rearange and added bug documentation

--- main.py
+++ main.py
@@ -7,34 +7,33 @@ from PySide6.QtGui import QCloseEvent
 app = QtWidgets.QApplication(sys.argv)
 width = app.primaryScreen().size().width()
 height = app.primaryScreen().size().height()
-windows = []
+windows = []    # list to put windows in to get around scoping
 
 
 class Head(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Hydra!")
+        self.setStyleSheet("background-color: black;")
         self.layout = QtWidgets.QVBoxLayout(self)
         self.text = QtWidgets.QLabel("Cut off one head . . .")
         self.text.setStyleSheet("color: white;")
         self.layout.addWidget(self.text)
-
-        self.setWindowTitle("Hydra")
-        self.setStyleSheet("background-color: black;")
         self.setFixedSize(200, 50)
         self.move(random.randint(0, width), random.randint(0, height))
-
         self.show()
 
     def closeEvent(self, _e: QCloseEvent):
         message = QtWidgets.QMessageBox()
+        message.setWindowTitle("Hydra!")
         message.setText("Two more shall take its place!")
         message.setStyleSheet("background-color: red;")
         message.exec()
 
         create_window()
         create_window()
-        windows.remove(self)
+        windows.remove(self)    # remove the old window to plug memory leak
 
 
 def create_window():
